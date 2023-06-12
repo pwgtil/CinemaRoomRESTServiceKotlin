fa980c1679648c71d9a00b5544c033a767b1ec06
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-06-12 15:54:48 +0200

    Cinema Room REST Service (Kotlin): Stage 3/4: A change of plans v3.0

--- Cinema Room REST Service (Kotlin)/task/src/cinema/Controller.kt
+++ Cinema Room REST Service (Kotlin)/task/src/cinema/Controller.kt
@@ -1,15 +1,22 @@
 package cinema
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+const val ERROR_MSG_SEAT_OUT_OF_BOUNDS = "The number of a row or a column is out of bounds!"
+const val ERROR_MSG_SEAT_TAKEN = "The ticket has been already purchased!"
+const val ERROR_MSG_WRONG_TOKEN = "Wrong token!"
 
 @RestController
 class Controller {
     private val cinema: Cinema = Cinema(9, 9)
+    private val soldSeats: MutableList<SoldSeat> = mutableListOf()
 
     @GetMapping("/seats")
     fun getInfo(): Cinema {
@@ -18,21 +25,45 @@ class Controller {
 
     @PostMapping("purchase")
     fun bookTicket(@RequestBody seat: Seat): ResponseEntity<Any> {
-        return if (seat.row !in 1..cinema.rows || seat.column !in 1..cinema.columns) {
-            ResponseEntity(object {
-                val error = "The number of a row or a column is out of bounds!"
-            }, HttpStatus.BAD_REQUEST)
-        } else {
-            val output = cinema.seats.find { it.row == seat.row && it.column == seat.column }
-                ?: object {
-                    val error = "The ticket has been already purchased!"
-                }
-            if (output is PricedSeat) {
-                cinema.seats.remove(output)
-                ResponseEntity(output, HttpStatus.OK)
-            } else {
-                ResponseEntity(output, HttpStatus.BAD_REQUEST)
-            }
+
+        // check if request out of bounds
+        if (seat.row !in 1..cinema.rows || seat.column !in 1..cinema.columns) {
+            return ResponseEntity(
+                object {
+                    val error = ERROR_MSG_SEAT_OUT_OF_BOUNDS
+                }, HttpStatus.BAD_REQUEST
+            )
         }
+
+        // check if seat is already taken
+        val seat = cinema.seats.find { it.row == seat.row && it.column == seat.column } ?: return ResponseEntity(
+            object {
+                val error = ERROR_MSG_SEAT_TAKEN
+            }, HttpStatus.BAD_REQUEST
+        )
+
+        // all seems ok, let's purchase the ticket and generate token
+        val token = UUID.randomUUID().toString()
+        val soldSeat = SoldSeat(token, seat)
+        cinema.seats.remove(seat)
+        soldSeats.add(soldSeat)
+        return ResponseEntity(soldSeat, HttpStatus.OK)
+    }
+
+    @PostMapping("return")
+    fun returnTicket(@RequestBody token: SoldSeat): ResponseEntity<Any> {
+
+        // check if token exists
+        val soldSeat = soldSeats.find { it.token == token.token && it.ticket != null }
+            ?: return ResponseEntity(object {
+                val error = ERROR_MSG_WRONG_TOKEN
+            }, HttpStatus.BAD_REQUEST)
+
+        soldSeats.remove(soldSeat)
+        cinema.seats.add(soldSeat.ticket!!)
+        return ResponseEntity(object {
+            @JsonProperty("returned_ticket")
+            val ticket = soldSeat.ticket
+        }, HttpStatus.OK)
     }
 }
--- Cinema Room REST Service (Kotlin)/task/src/cinema/Seat.kt
+++ Cinema Room REST Service (Kotlin)/task/src/cinema/Seat.kt
@@ -3,3 +3,5 @@ package cinema
 open class Seat(val row: Int, val column: Int)
 
 class PricedSeat(row: Int, column: Int, val price: Int) : Seat(row, column)
+
+class SoldSeat(val token: String, val ticket: PricedSeat?)
